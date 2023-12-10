ac1a0ad9bf0ea2f6e84af385b2badbcbe875014d
Fred Lahde <fred@kloud.software>
2023-12-10 17:59:08 +0100

    day09 refactor

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -6,14 +6,12 @@ fn main() {
     {
         let p = parse(INPUT);
         let ans: i32 = p.into_iter().map(|x| solve_line_forward(&x)).sum();
-        println!("p1 {ans}");
-        assert_eq!(2105961943, ans);
+        assert_eq!(2_105_961_943, ans);
     }
     {
         let p = parse(INPUT);
         let ans: i32 = p.into_iter().map(|x| solve_line_backward(&x)).sum();
-        println!("p2 {ans}");
-        assert_eq!(1019, ans);
+        assert_eq!(1_019, ans);
     }
 }
 
@@ -21,10 +19,13 @@ fn find_diffs(l: &[i32]) -> Vec<Vec<i32>> {
     let mut diffs = Vec::new();
     diffs.push(l.to_vec());
     loop {
-        let mut d = Vec::new();
-        for i in 1..diffs.last().unwrap().len() {
-            d.push(diffs.last().unwrap()[i] - diffs.last().unwrap()[i - 1])
-        }
+        let d: Vec<_> = diffs
+            .last()
+            .unwrap()
+            .windows(2)
+            .map(|w| w[1] - w[0])
+            .collect();
+
         diffs.push(d.clone());
         if d.into_iter().all(|x| x == 0) {
             break;
@@ -34,31 +35,37 @@ fn find_diffs(l: &[i32]) -> Vec<Vec<i32>> {
     diffs
 }
 
-fn solve_line_backward(l: &[i32]) -> i32 {
+#[inline]
+fn solve_line_intern<CalcFn, PushFn>(l: &[i32], calc_fn: CalcFn, push_fn: PushFn) -> i32
+where
+    CalcFn: Fn(&[i32], &[i32]) -> i32,
+    PushFn: Fn(&mut Vec<i32>, i32),
+{
     let mut diffs = find_diffs(l);
-
     let mut y = 0;
-    for i in (1..=diffs.len() - 1).rev() {
+    for i in (1..diffs.len()).rev() {
         let line = &diffs[i];
         let prev_line = &diffs[i - 1];
-        let x = prev_line.first().unwrap() - line.first().unwrap();
-        diffs[i - 1].insert(0, x);
+        let x = calc_fn(line, prev_line);
+        push_fn(&mut diffs[i - 1], x);
         y = x;
     }
     y
 }
 
+fn solve_line_backward(l: &[i32]) -> i32 {
+    solve_line_intern(
+        l,
+        |a, b| *b.first().unwrap() - *a.first().unwrap(),
+        |v, x| v.insert(0, x),
+    )
+}
 fn solve_line_forward(l: &[i32]) -> i32 {
-    let mut diffs = find_diffs(l);
-    let mut y = 0;
-    for i in (1..=diffs.len() - 1).rev() {
-        let line = &diffs[i];
-        let prev_line = &diffs[i - 1];
-        let x = line.last().unwrap() + prev_line.last().unwrap();
-        diffs[i - 1].push(x);
-        y = x;
-    }
-    y
+    solve_line_intern(
+        l,
+        |a, b| *a.last().unwrap() + *b.last().unwrap(),
+        std::vec::Vec::push,
+    )
 }
 
 fn parse(s: &str) -> Vec<Vec<i32>> {
